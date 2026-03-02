eb5bf12df97f0cdcaa4dabf532f64a460901e035
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-03-03 00:12:00 +0500

    Initial commit

---
+++ Matrix/LuckyNumber_Problem#1380.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Matrix
+{
+    public class LuckyNumber_Problem_1380
+    {
+        public IList<int> LuckyNumbers(int[][] matrix)
+        {
+            List<int> luckyNumbers = new List<int>();
+
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                int min = matrix[row].Min();
+                int colIndex = Array.IndexOf(matrix[row], min);
+                List<int> columnValues = new List<int>();
+                for (var col = 0; col < matrix.Length; col++)
+                {
+                    columnValues.Add(matrix[col][colIndex]);
+                }
+                if (columnValues != null && columnValues.Any() && columnValues.Max() == min)
+                    luckyNumbers.Add(min);
+            }
+            return luckyNumbers;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Matrix;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new LuckyNumber_Problem_1380();
+        var result = solution.LuckyNumbers([[7, 8], [1, 2]]);
+
+        Console.WriteLine("the result is",string.Join(",", result));
+        Console.WriteLine("the result is {0}", result);
+    }
+}
