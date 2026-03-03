5a10712517b4128350a7f853b4cbd3fe4ec0515c
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-03-03 23:32:45 +0500

    add matrix diognal sum problem

---
+++ Matrix/MatrixDiognalSum_Problem#1572.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Matrix
+{
+    public class MatrixDiognalSum_Problem_1572
+    {
+        public int DiagonalSum(int[][] mat)
+        {
+            bool isEven = mat.Length % 2 == 0;
+            var sum = 0;
+            var crossIndex = 0;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                sum += mat[i][i];
+            }
+            for (int j = mat.Length - 1; j >= 0; j--)
+            {
+                if (!isEven && j == mat.Length / 2)
+                {
+                    crossIndex++;
+                    continue;
+                }
+                else
+                    sum += mat[j][crossIndex];
+                crossIndex++;
+            }
+            return sum;
+        }
+    }
+}
