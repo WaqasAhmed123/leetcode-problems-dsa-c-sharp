2516f1825a1655ca4fcd15274412fa315adbfb16
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-03-07 23:19:05 +0500

    add files for linked list

---
+++ Linked List/MergeTwoSortedLists_Problem_21.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Linked_List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Linked_List
+{
+    public class MergeTwoSortedLists_Problem_21
+    {
+        //public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        //{
+
+        //}
+
+    }
+}
