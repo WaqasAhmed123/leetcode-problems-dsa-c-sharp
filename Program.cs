23724583bcb947fe32a71b1d185e4acb9f03f773
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-03-17 23:15:40 +0500

    add merge two sorted linked lists

--- Program.cs
+++ Program.cs
@@ -5,14 +5,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        //var solution = new LuckyNumber_Problem_1380();
-        //var result = solution.LuckyNumbers([[7, 8], [1, 2]]);
 
-        var solution = new BasicLinkedListOperations();
-        ListNode n4 = new ListNode(5); ListNode n3 = new ListNode(4, n4); ListNode n2 = new ListNode(2, n3); ListNode n1 = new ListNode(1, n2);
-        //solution.AddAtLast(n1,5);
-        //solution.DeleteAtLast(n1);
-        solution.AddAfter(n1,3,2);
+        var solution = new MergeTwoSortedLists_Problem_21();
+        // list1: 1 -> 2 -> 4
+        ListNode l1_3 = new ListNode(4);
+        ListNode l1_2 = new ListNode(2, l1_3);
+        ListNode l1_1 = new ListNode(1, l1_2);
+
+        // list2: 1 -> 3 -> 4
+        ListNode l2_3 = new ListNode(4);
+        ListNode l2_2 = new ListNode(3, l2_3);
+        ListNode l2_1 = new ListNode(1, l2_2);
+
+        ListNode result = solution.MergeTwoLists(l1_1, l2_1);
 
         //Console.WriteLine("the result is",string.Join(",", result));
         //Console.WriteLine("the result is {0}", result);
