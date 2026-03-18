58311484e64fbdf083b89dfac68f8fb8ee0676d9
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-03-18 23:21:08 +0500

    in-progress remove duplicates in linkedlist

---
+++ Linked List/Remove_Duplicates_from_Sorted_List_Problem_83.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Linked_List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Linked_List
+{
+    public class Remove_Duplicates_from_Sorted_List_Problem_83
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            visited.Add(head.val);
+            ListNode current = head;
+            while (current.next != null)
+            {
+                if (visited.Contains(current.next.val))
+                {
+
+                    while (visited.Contains(current.next.next.val))
+                    //while (current.next.next != null && !visited.Contains(current.next.next.val))
+                    {
+                        current.next = current.next.next;
+                    }
+                    //if (visited.Contains(current.next.val))
+                    //{
+                    //    current.next = current.next.next;
+                    //}
+                }
+                else { visited.Add(current.next.val); }
+                Console.WriteLine("the result is {0}", current.val);
+                current = current.next;
+            }
+            if (visited.Contains(current.val))
+            {
+                current.next = null;
+            }
+            Console.WriteLine("the result is {0}", current.val);
+            return current;
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,18 +6,20 @@ class Program
     static void Main(string[] args)
     {
 
-        var solution = new MergeTwoSortedLists_Problem_21();
+        var solution = new Remove_Duplicates_from_Sorted_List_Problem_83();
         // list1: 1 -> 2 -> 4
-        ListNode l1_3 = new ListNode(4);
+        ListNode l1_5 = new ListNode(4);
+        ListNode l1_4 = new ListNode(1,l1_5);
+        ListNode l1_3 = new ListNode(2,l1_4);
         ListNode l1_2 = new ListNode(2, l1_3);
         ListNode l1_1 = new ListNode(1, l1_2);
 
         // list2: 1 -> 3 -> 4
         ListNode l2_3 = new ListNode(4);
-        ListNode l2_2 = new ListNode(3, l2_3);
+        ListNode l2_2 = new ListNode(3, l2_3);  
         ListNode l2_1 = new ListNode(1, l2_2);
 
-        ListNode result = solution.MergeTwoLists(l1_1, l2_1);
+        ListNode result = solution.DeleteDuplicates(l1_1);
 
         //Console.WriteLine("the result is",string.Join(",", result));
         //Console.WriteLine("the result is {0}", result);
