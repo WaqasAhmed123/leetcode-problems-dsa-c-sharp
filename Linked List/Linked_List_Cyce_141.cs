78e7634e8f6e064c26632b300e5a5d51aec22fa4
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-04-20 07:18:01 +0500

    add problem linked list -- Linked_List_Cyce_141

---
+++ Linked List/Linked_List_Cyce_141.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Linked_List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Linked_List
+{
+    public class Linked_List_Cyce_141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+            var current = head;
+            HashSet<ListNode> nodesVisited = new HashSet<ListNode>();
+            while (current.next != null)
+            {
+                if (nodesVisited.Contains(current.next))
+                {
+                    return true;
+                }
+                nodesVisited.Add(current.next);
+                current = current.next;
+            }
+            return false;
+        }
+    }
+}
