99e2c56bbaf287385258219f703f4af2eb0f767a
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-05-14 08:59:12 +0500

    in-progress reverse linked list

---
+++ Linked List/ReverseLinkedList_Problem_206.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Linked_List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Linked_List
+{
+    public class ReverseLinkedList_Problem_206
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode current = head;
+            ListNode updated_head = new ListNode(current.next.val);
+            while (current.next != null)
+            {
+                updated_head.next = new ListNode(current.val);
+                updated_head = updated_head.next;
+                //updated_head.next = current.;
+                //head = current.next;
+                //updated_head.next = current;
+                //head.next = current;
+                current = current.next;
+
+            }
+
+            return updated_head;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,47 @@
 ﻿using LeetCode.Linked_List;
-using LeetCode.Matrix;
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        var solution = new Remove_Duplicates_from_Sorted_List_Problem_83();
+        var solution = new ReverseLinkedList_Problem_206();
         // list1: 1 -> 2 -> 4
-        ListNode l1_5 = new ListNode(4);
-        ListNode l1_4 = new ListNode(1,l1_5);
-        ListNode l1_3 = new ListNode(2,l1_4);
+        ListNode l1_5 = new ListNode(5, null);
+        ListNode l1_4 = new ListNode(4, l1_5);
+        ListNode l1_3 = new ListNode(3, l1_4);
         ListNode l1_2 = new ListNode(2, l1_3);
         ListNode l1_1 = new ListNode(1, l1_2);
 
         // list2: 1 -> 3 -> 4
-        ListNode l2_3 = new ListNode(4);
-        ListNode l2_2 = new ListNode(3, l2_3);  
-        ListNode l2_1 = new ListNode(1, l2_2);
+        ListNode l2_5 = new ListNode(4);
+        ListNode l2_4 = new ListNode(8, l2_5);
+        ListNode l2_3 = new ListNode(1, l1_3);
+        //ListNode l2_3 = new ListNode(1, l2_4);
+        ListNode l2_2 = new ListNode(6, l2_3);
+        ListNode l2_1 = new ListNode(5, l2_2);
 
-        ListNode result = solution.DeleteDuplicates(l1_1);
+        //create a cylce:
+        //ListNode l2_1 = new ListNode(1);
+        //ListNode l2_2 = new ListNode(3);
+        //ListNode l2_3 = new ListNode(4);
 
-        //Console.WriteLine("the result is",string.Join(",", result));
-        //Console.WriteLine("the result is {0}", result);
+        //// Step 2: Link them normally
+        //l2_1.next = l2_2;
+        //l2_2.next = l2_3;
+
+        //// Step 3: Create the cycle
+        //l2_3.next = l2_2;  // cycle here (node 4 points back to node 3)
+
+        ListNode result = solution.ReverseList(l1_1);
+
+        if (result != null)
+        {
+            Console.WriteLine("The updated list's head value is {0}", result.val);
+        }
+        else
+        {
+            Console.WriteLine("No intersection found.");
+        }
     }
 }
