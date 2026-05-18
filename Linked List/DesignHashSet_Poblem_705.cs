d45159969eb1ea1c6f3a6643a9d681e34aecbce2
Waqas Ahmed <waqasahmed2790@gmail.com>
2026-05-18 08:33:45 +0500

    add linked list -> design hash set

---
+++ Linked List/DesignHashSet_Poblem_705.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Linked_List;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace LeetCode.Linked_List
+{
+    public class MyHashSet
+    {
+        List<int> list = new List<int>();
+
+        public MyHashSet()
+        {
+
+        }
+
+        public void Add(int key)
+        {
+            if (!list.Contains(key))
+            {
+                list.Add(key);
+            }
+        }
+
+        public void Remove(int key)
+        {
+            list.Remove(key);
+        }
+
+        public bool Contains(int key)
+        {
+            return list.Contains(key);
+        }
+
+
+
+
+        /**
+         * Your MyHashSet object will be instantiated and called as such:
+         * MyHashSet obj = new MyHashSet();
+         * obj.Add(key);
+         * obj.Remove(key);
+         * bool param_3 = obj.Contains(key);
+         */
+    }
+}
